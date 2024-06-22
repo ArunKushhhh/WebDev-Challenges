f339b39f1a5936bd76a394cb43c0f8dd04d96953
ArunKushhhh <arunkushwaha_230604@gmail.edu.in>
2024-06-22 14:09:46 +0530

    Progress Bar Project added

---
+++ 25.Progress-bar/app.js
@@ -0,0 +1,58 @@
+//progress
+const progress = document.getElementById("progress");
+//prev btn
+const prevBtn = document.getElementById("prev");
+//next btn
+const nextBtn = document.getElementById("next");
+//circles
+const circles = document.querySelectorAll(".circle");
+
+// console.log(progress);
+// console.log(prevBtn);
+// console.log(nextBtn);
+// console.log(circles);
+
+let activeIndex = 1;
+
+nextBtn.addEventListener("click", () => {
+  activeIndex++;
+  if (activeIndex > circles.length) {
+    activeIndex = circles.length;
+  }
+
+  updateUI();
+});
+
+prevBtn.addEventListener("click", () => {
+  activeIndex--;
+  if (activeIndex < 1) {
+    activeIndex = 1;
+  }
+
+  updateUI();
+});
+
+function updateUI() {
+  circles.forEach((circle, index) => {
+    if (index < activeIndex) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (activeIndex === 1) {
+    prevBtn.disabled = true;
+    nextBtn.disabled = false;
+  } else if (activeIndex === circles.length) {
+    nextBtn.disabled = true;
+    prevBtn.disabled = false;
+  } else {
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+  }
+}
