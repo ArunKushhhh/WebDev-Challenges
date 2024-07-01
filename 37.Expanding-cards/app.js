8bb167077bee10f601abddc17e447feabf6602e1
ArunKushhhh <arunkushwaha_230604@gmail.edu.in>
2024-07-01 19:50:00 +0530

    Expanding Cards Project Added

---
+++ 37.Expanding-cards/app.js
@@ -0,0 +1,15 @@
+const slides = document.querySelectorAll(".slide");
+// console.log(slides);
+
+slides.forEach((slide) => {
+  slide.addEventListener("click", () => {
+    removeCurrentActives();
+    slide.classList.add("active");
+  });
+});
+
+function removeCurrentActives() {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+}
