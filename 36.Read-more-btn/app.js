6bc7da16c3f44dbb3e79e479b3477701391d62ef
ArunKushhhh <arunkushwaha_230604@gmail.edu.in>
2024-07-01 18:31:13 +0530

    Read more btn project added

---
+++ 36.Read-more-btn/app.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".btn");
+const content = document.querySelector(".content-container-2");
+
+btn.addEventListener("click", () => {
+    content.classList.toggle("toggle")
+});
