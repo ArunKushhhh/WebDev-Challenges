21cc7353d9198063de745a858f3c1cceb2fe68e8
ArunKushhhh <arunkushwaha_230604@gmail.edu.in>
2024-07-01 19:51:53 +0530

    KeyCode project added

---
+++ 38.keyCode/app.js
@@ -0,0 +1,19 @@
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `
+    <div class = "key"> 
+    ${e.key === "" ? "Space" : e.key}
+    <small>keyName </small>
+    </div>
+    <div class = "key"> 
+    ${e.keyCode}
+    <small>e.keyCode </small>
+    </div>
+    <div class = "key"> 
+    ${e.code}
+    <small>e.code </small>
+    </div>
+
+    `;
+});
