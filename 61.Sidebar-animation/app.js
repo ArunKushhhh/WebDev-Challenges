606c9b9b91c07aa0bdb3825c08b960eaf76a79d9
ArunKushhhh <arunkushwaha_230604@gmail.edu.in>
2024-07-26 23:02:03 +0530

    Password Strength project added

--- 61.Sidebar-animation/app.js
+++
@@ -1,9 +0,0 @@
-
-const menuBtn = document.getElementById("menu");
-const sidebar = document.getElementById("sidebar");
-const content = document.getElementById("content");
-
-menuBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-  content.classList.toggle("active");
-});
